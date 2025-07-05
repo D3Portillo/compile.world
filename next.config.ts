521dc23f9853b4129487089f88b16374cec31d22
D3Portillo <d3portillo@gmail.com>
2025-07-06 01:12:47 +0200

    chores: basic setup done

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import nextra from "nextra"
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// Set up Nextra with its configuration
+const withNextra = nextra({
+  search: {
+    codeblocks: false, // Disable search in code blocks
+  },
+})
 
-export default nextConfig;
+// Export the final Next.js config with Nextra included
+export default withNextra({
+  // ... Add regular Next.js options here
+})
