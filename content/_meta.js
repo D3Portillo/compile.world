f4e0611b885977358c189177cecf98753baa429c
D3Portillo <d3portillo@gmail.com>
2025-07-10 10:05:38 +0200

    chores: init - some good shi

---
+++ content/_meta.js
@@ -0,0 +1,15 @@
+export default {
+  index: "👋 Welcome",
+  "mini-apps-101": "Mini Apps 101",
+  "your-first-mini-app": "Your First Mini App",
+  "---": {
+    type: "separator",
+  },
+  troubleshooting: "⚙️ Troubleshooting",
+  guides: "📒 Guides",
+  "ai-rules": "🤖 AI Rules",
+  "###": {
+    type: "separator",
+  },
+  "need-help": "Need Help?",
+}
---
+++ content/mini-apps-101/_meta.js
@@ -0,0 +1,5 @@
+export default {
+  worldchain: "Worldchain",
+  "world-app": "World App",
+  minikit: "Mini Kit",
+}
