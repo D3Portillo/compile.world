586f5097baed7698394bcfdb7ba2d5c0c2246bf1
D3Portillo <d3portillo@gmail.com>
2025-07-11 09:51:29 +0200

    chores: add minin app setup

--- content/your-first-mini-app/_meta.js
+++ content/your-first-mini-app/_meta.js
@@ -1,6 +1,7 @@
 export default {
   index: "Getting Started",
   "dev-setup": "Initial Setup",
+  "loading-app": "View Your App",
   "user-data": "Fetching User Data",
   payments: "Handling Payments",
 }
