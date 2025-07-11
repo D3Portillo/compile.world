60c2b1cbd70f87ea0420628d1dc2331fcfd650c8
D3Portillo <d3portillo@gmail.com>
2025-07-11 10:22:51 +0200

    chores: adding demo

--- content/your-first-mini-app/_meta.js
+++ content/your-first-mini-app/_meta.js
@@ -2,6 +2,7 @@ export default {
   index: "Getting Started",
   "dev-setup": "Initial Setup",
   "loading-app": "View Your App",
+  "whats-this": "Coding 🥺?",
   "user-data": "Fetching User Data",
   payments: "Handling Payments",
 }
